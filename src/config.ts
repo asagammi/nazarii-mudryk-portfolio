6838b8851286ef3031d8e42b7c5ac303fdfa1174
Nazarii Mudryk <159130263+asagammi@users.noreply.github.com>
2026-09-25 17:27:45 +0300

    Add Upwork profile URL

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ export const profile = {
   email: 'asagammi@gmail.com',
   links: {
     facebook: 'https://www.facebook.com/mudryk.nazar/',
+    upwork: 'https://www.upwork.com/freelancers/~018c454d8e59342ab4',
     cv: 'https://drive.google.com/file/d/1Et5YJrfyejLJv9SDHO4D8a9ytE0hRmtC/view?usp=sharing',
     linkedin: 'https://www.linkedin.com/in/nazarii-mudryk/',
     github: 'https://github.com/asagammi',
